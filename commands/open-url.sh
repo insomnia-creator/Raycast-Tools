24a17275a3a205da641b8af496227bc998568f7e
CorsairRGB 16gb DDR4 3200MHz RAM <69950985+insomnia-creator@users.noreply.github.com>
2021-09-01 11:58:36 +0530

    new command + reforms

---
+++ commands/open-url.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open URL
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌏
+# @raycast.argument1 { "type": "text", "placeholder": "URL?" }
+
+# Documentation:
+# @raycast.description Opens a URL in your browser!
+# @raycast.author CorsairRGB 16gb DDR4 3200MHz RAM
+# @raycast.authorURL https://github.com/insomnia-creator
+
+open $1
